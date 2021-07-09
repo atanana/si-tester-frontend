079059080d6fc72c371a04b2c18abdabb09fbc0f
Andrei Tanana <tanana@email.ru>
2021-07-09 15:57:47 +0300

    websockets init

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 import { getName, setName } from "./storage";
+import { WebsocketBuilder } from 'websocket-ts';
 
-let name = getName()
+let name = getName();
 while (!name) {
-    name = prompt("Представься, мразь")
-    setName(name)
+    name = prompt("Представься, мразь");
+    setName(name);
 }
+
+const ws = new WebsocketBuilder('ws://localhost:8080/ws')
+    .onOpen((_, event) => { })
+    .onError((_, event) => { alert('Произошла чудовищная ошибка!') })
+    .onClose((_, event) => { alert('Утрачено соединение с сервером!') })
+    .onMessage((_, event) => { console.log(event.data) })
+    .build();
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === ' ') {
+        ws.send("action");
+    }
+});
